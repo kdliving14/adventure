cb65bd836e7fa25ce043253b2c34d5704f76a2e5
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-10-11 14:53:56 -0700

    set up for heroku (change ruby version and re-install rails)

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  resources :userchoices, only: [:index, :show, :create, :destroy]
-  resources :choices, only: [:index, :show]
-  resources :inventories, only: [:index, :create]
-  resources :items, only: [:index]
-  resources :events, only: [:index, :show]
-  resources :userstories, only: [:index, :show, :create, :destroy]
-  resources :stories, only: [:index]
-  resources :users, only: [:create, :destroy]
-  
-  post "/login", to: "sessions#create" 
-  delete "/logout", to: "sessions#destroy"
+    resources :userchoices, only: [:index, :show, :create, :destroy]
+    resources :choices, only: [:index, :show]
+    resources :inventories, only: [:index, :create]
+    resources :items, only: [:index]
+    resources :events, only: [:index, :show]
+    resources :userstories, only: [:index, :show, :create, :destroy]
+    resources :stories, only: [:index]
+    resources :users, only: [:create, :destroy]
+    
+    post "/login", to: "sessions#create" 
+    delete "/logout", to: "sessions#destroy"
 
-  patch "/chosen", to: "userstories#choose"
+    patch "/chosen", to: "userstories#choose"
 
-  get "/me", to: "users#show"
-
+    get "/me", to: "users#show"
 end
