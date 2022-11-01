b539b0e40fab4f9761ce0ade70528609963a5a58
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-11-01 12:03:54 -0700

    change icons

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Hopefully I can get some writers to help add more stories, but there is one stor
 - CSS Framework: [Tailwind](https://tailwindcss.com/) & [Flowbite](https://flowbite.com/)
 - Sounds: [Youtube Audio library](https://support.google.com/youtube/answer/3376882?hl=en-GB)
 - Photos: [unSplash](https://unsplash.com/)
+- Icons: [HeroIcons](https://heroicons.com/)
 - Logos for Dev, Github, & LinkedIn: Edited origninal logos to fit theme, all rights to original copyholders.
 - Other Images: Created by myself in [Illustrator](https://www.adobe.com/products/illustrator.html) & [Photoshop](https://www.adobe.com/products/photoshop.html)
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,3 @@
-// import logo from './logo.svg';
-// <img src={logo} className="App-logo" alt="logo" />
-import Home from "./images/home.png"
-import Story from "./images/story.png"
-import User from "./images/user.png"
-import Logout from "./images/logout.png"
-
 
 import { useNavigate } from "react-router-dom";
 // import useSound from 'use-sound';
@@ -27,17 +20,24 @@ function Navbar({setCurrentUser, image}) {
           <nav className="pt-2">
             <div className="flex items-center">
                 <a className="m-auto bg-transparent cursor-pointer" href={`/`}>
-                    <img src={Home} alt="Home" className="w-8"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-8 h-8">
+                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                  </svg> 
                 </a>
                 <a className="m-auto bg-transparent cursor-pointer" href={`/story`}> 
-                  <img src={Story} alt="Story" className="h-7"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-8 h-8">
+                    <path d="M11.25 4.533A9.707 9.707 0 006 3a9.735 9.735 0 00-3.25.555.75.75 0 00-.5.707v14.25a.75.75 0 001 .707A8.237 8.237 0 016 18.75c1.995 0 3.823.707 5.25 1.886V4.533zM12.75 20.636A8.214 8.214 0 0118 18.75c.966 0 1.89.166 2.75.47a.75.75 0 001-.708V4.262a.75.75 0 00-.5-.707A9.735 9.735 0 0018 3a9.707 9.707 0 00-5.25 1.533v16.103z" />
+                  </svg>
                 </a>
                 
                 <a className="m-auto bg-transparent cursor-pointer" href={`/profile`}> 
                   <div className="m-auto">
                     {image==="" ? 
                       <div className="inline-flex items-center">
-                        <img src={User} alt="User" className="w-9 pt-1"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-8 h-8">
+                          <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                        </svg>
                       </div> 
                       : 
                       <img className="m-auto h-9 rounded-full" src={image} alt="user"/>
@@ -45,7 +45,9 @@ function Navbar({setCurrentUser, image}) {
                   </div>
                 </a>
                 <button type="button" onClick={handleLogout} className="m-auto w-8 cursor-pointer">
-                  <img src={Logout} alt="logout" />
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-8 h-8">
+                    <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z" clip-rule="evenodd" />
+                  </svg>
                 </button>
             </div>
           </nav>
